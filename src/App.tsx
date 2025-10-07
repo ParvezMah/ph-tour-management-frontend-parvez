c95117d16077f35b09e3e67adf25821070d9fd5b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-07 15:25:37 +0600

    Project Setup complete

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1>PH Tour Management Frontend</h1>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
